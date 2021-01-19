8cd595239833f1352653485dd66a9512b4ca07cb
cptvictor95 <cpt.victor@hotmail.com>
2021-01-19 17:13:51 -0300

    detalhesPage created, font imports done

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
+import { Flex, Heading, Box } from "@chakra-ui/react";
+import DetalhesPage from "./pages/DetalhesPage";
 
-import { Flex } from "@chakra-ui/react";
 export class AppContainer extends Component {
   render() {
     return (
-      <Flex>
-        <p>hello world!</p>
+      <Flex
+        h="100vh"
+        w="100vw"
+        direction="column"
+        align="center"
+        justify="center"
+        background="#F0FAF7"
+        fontFamily="'Montserrat', sans-serif;"
+      >
+        <Heading
+          fontFamily="'Poppins', sans-serif;"
+          fontWeight="900"
+          color="#EF6F6C"
+          fontSize="64px"
+          as="h1"
+        >
+          FUTURECAR
+        </Heading>
+
+        <Heading color="#000" fontSize="64px" as="h1">
+          going beyond
+        </Heading>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+          consequatur impedit praesentium quisquam maxime debitis maiores
+          placeat optio porro fugit eaque sit, qui natus consectetur eveniet quo
+          nam quos? Facere?
+        </p>
+        <DetalhesPage />
       </Flex>
     );
   }
